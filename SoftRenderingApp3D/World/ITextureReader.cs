ec6c88dfc873a271a2a1355a36f67a896923a6df
DESKTOP-ML07PTV\Kiril Avramoski <kiril.avramoski@yahoo.com>
2023-08-22 15:31:02 +0200

    Created TextureReader interface and Texture class.

---
+++ SoftRenderingApp3D/World/ITextureReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms3D.World {
+    interface ITextureReader {
+
+        public void ReadImage(string path);
+        
+    }
+}
---
+++ SoftRenderingApp3D/World/TextureReaderBMP.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms3D.World {
+    class TextureReaderBMP : ITextureReader {
+        public void ReadImage(string path) {
+            throw new NotImplementedException();
+        }
+    }
+}
