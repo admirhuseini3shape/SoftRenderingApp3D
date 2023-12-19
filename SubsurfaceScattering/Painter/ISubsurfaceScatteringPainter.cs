2aa4c4cc7fdb3e5739e32caf6fdd866e3729c8bc
Kiril Avramoski <Kiril.Avramoski@3Shape.com>
2023-12-19 15:51:14 +0100

    Removed redundant files.

--- SubsurfaceScattering/Painter/ISubsurfaceScatteringPainter.cs
+++
@@ -1,8 +0,0 @@
-﻿using SoftRenderingApp3D.Buffer;
-
-namespace SubsurfaceScattering.Painter {
-    public interface ISubsurfaceScatteringPainter {
-        SubsurfaceScatteringRenderContext RendererContext { get; set; }
-        void DrawTriangle(VertexBuffer vbx, int triangleIndex);
-    }
-}
--- SubsurfaceScattering/World/SubsurfaceScatteringFileReader.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SubsurfaceScattering.World {
-    public abstract class SubsurfaceScatteringFileReader {
-        public abstract IEnumerable<SubsurfaceScatteringVolume> ReadFile(string fileName);
-    }
-}
