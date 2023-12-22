ad552301b02de0405bbf6ab29b3499101fefcbe1
Art Saiti <hatshepsut@mailfence.com>
2023-12-22 02:42:40 +0100

    Removed LINQ method in VolumeEx.cs and replaced it with a standard for-loop.

--- SoftRenderingApp3D/DataStructures/VolumeEx.cs
+++ SoftRenderingApp3D/DataStructures/VolumeEx.cs
@@ -20,7 +20,10 @@ namespace SoftRenderingApp3D.DataStructures {
             var inTriangles = GetTriangleIndexesHaving(vertex, volume);
             if(!inTriangles.Any())
                 return Vector3.Zero;
-            var sum = inTriangles.Select(idx => volume.Triangles[idx].CalculateNormal(volume.Vertices)).Distinct().Aggregate((v1, v2) => v1 + v2);
+            Vector3 sum = new Vector3(0, 0, 0); 
+            foreach (var idx in inTriangles) {
+                sum += volume.Triangles[idx].CalculateNormal(volume.Vertices);
+            }
             return Vector3.Normalize(sum);
         }
 
